59d1de217a4fb28fb7a138d9fa4b8a1d71d2bbbb
Angelo Segretto IV <77angelsecret77@gmail.com>
2023-04-06 22:59:40 +0000

    milestone correction

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -76,13 +76,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mysite',
-        'USER': 'postgres',
-        'PASSWORD': 'django',
-        'HOST': 'localhost',
-    },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 
 # Password validation
