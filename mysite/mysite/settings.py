c682163fa725adf5e682947ea001840bec4b6103
Angelo Segretto IV <77angelsecret77@gmail.com>
2023-04-13 23:10:21 +0000

    docker builds in github

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,13 +75,10 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mysite',
-        'USER': 'postgres',
-        'PASSWORD': 'django',
-        'HOST': 'localhost',
-    },
+   'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
