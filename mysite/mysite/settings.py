b8bcb9d4ed9b1479072f84c8ee9e05cd662b3059
Angelo Segretto IV <77angelsecret77@gmail.com>
2023-05-29 20:23:24 +0000

    settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [os.environ.get('HOSTNAME', 'localhost')]
 
 
 # Application definition
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'mysite',
         'USER': 'postgres',
         'PASSWORD': 'django',
-        'HOST': [hostname],
+        'HOST': 'postgres',
     },
 }
 
