2db6c115bef690be4a26d60e9e8bd86a5e785479
Angelo Segretto IV <77angelsecret77@gmail.com>
2023-05-30 03:58:35 +0000

    secret key

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,7 @@ CSRF_TRUSTED_ORIGINS = ['https://*.run.app']
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-@o71s+_^+tm0*33j7gv3xrjjtue5)z55sjg40urg(7pu*8kl)5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
