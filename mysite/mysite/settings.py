8f26a64f1976d73ab847baf7896973bcf52c19f5
Angelo Segretto IV <77angelsecret77@gmail.com>
2023-05-30 04:07:19 +0000

    setting

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'USER': os.environ['DB_USER'],
         'PASSWORD': os.environ['DB_PASSWORD'],
         'HOST': os.environ['DB_HOST'],
-        'PORT': os.environ['DB_PORT'],
+        'PORT': os.environ['PORT'],
     },
 }
 
